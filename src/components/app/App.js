74fe49e2ca3bd9e5c72cc5bbc24a4e97411d001e
ppavankumar727 <ppavankumar727@gmail.com>
2021-09-19 23:31:18 +0530

    empty navbar , imported react-router-dom

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from '../navbar/navbar';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Nav></Nav>
+      <Router>
+      </Router>
     </div>
   );
 }
-
+ 
 export default App;
